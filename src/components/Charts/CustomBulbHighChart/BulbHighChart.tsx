f81fbb3f24b122630c5198b04f32da46e838547b
Suraj gm <suraajgm@gmail.com>
2024-05-20 13:54:59 +0545

    Updated container logic for bulb chart

--- src/components/Charts/CustomBulbHighChart/BulbHighChart.tsx
+++ src/components/Charts/CustomBulbHighChart/BulbHighChart.tsx
@@ -1,7 +1,11 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsPictorial from 'highcharts/modules/pictorial';
 import HighchartsAccessibility from 'highcharts/modules/accessibility';
+
+// Initialize Highcharts modules
+HighchartsPictorial(Highcharts);
+HighchartsAccessibility(Highcharts);
 
 const bulbHighData = [
   {
@@ -28,73 +32,70 @@ const bulbHighData = [
 
 export const BulbHighChart = () => {
   useEffect(() => {
-    HighchartsPictorial(Highcharts);
-    HighchartsAccessibility(Highcharts);
-
-    Highcharts.chart('bulbContainer', {
-      chart: {
-        type: 'pictorial',
-        margin: [40, 0, 10, 0],
-      },
-      colors: bulbHighData.map((bulb) => bulb.color),
-      title: {
-        text: 'Street Light Powered',
-        align: 'left',
-        style: {
-          color: '#475467',
-          fontSize: '14px',
-          fontWeight: 'bold',
+    const container = document.getElementById('bulbContainer');
+    if (container) {
+      Highcharts.chart(container, {
+        chart: {
+          type: 'pictorial',
+          margin: [40, 0, 10, 0],
         },
-      },
-      xAxis: {
-        visible: false,
-        // min: 0.2,
-      },
-      yAxis: {
-        visible: false,
-      },
-      legend: {
-        // enabled: false,
-        align: 'right',
-        floating: true,
-        itemMarginTop: 5,
-        itemMarginBottom: 5,
-        layout: 'vertical',
-        margin: 0,
-        padding: 0,
-        verticalAlign: 'middle',
-      },
-      tooltip: {
-        // enabled: false,
-        headerFormat: '',
-        valueSuffix: '%',
-      },
-      credits: {
-        enabled: false, // Disable Highcharts.com tag
-      },
-      plotOptions: {
-        series: {
-          pointPadding: 0,
-          groupPadding: 0,
-          borderWidth: 1,
-          borderColor: '#1D2939',
-          dataLabels: {
-            enabled: true,
-            align: 'center',
-            format: `{y}%`,
+        colors: bulbHighData.map((bulb) => bulb.color),
+        title: {
+          text: 'Street Light Powered',
+          align: 'left',
+          style: {
+            color: '#475467',
+            fontSize: '14px',
+            fontWeight: 'bold',
           },
-          stacking: 'percent',
-          paths: [
-            {
-              definition:
-                'M206.452 216.735C231.945 194.011 248 160.915 248 124.067 248 55.547 192.483 0 124 0S0 55.547 0 124.067c0 36.664 15.895 69.613 41.166 92.325 8.496 17.804 14.781 35.067 15.346 54.453.246 8.451 6.678 15.92 15.134 15.92h105.832c8.58 0 15.082-7.689 15.312-16.266.474-17.711 5.863-35.274 13.662-53.764z',
-            },
-          ],
         },
-      },
-      series: bulbHighData,
-    });
+        xAxis: {
+          visible: false,
+        },
+        yAxis: {
+          visible: false,
+        },
+        legend: {
+          align: 'right',
+          floating: true,
+          itemMarginTop: 5,
+          itemMarginBottom: 5,
+          layout: 'vertical',
+          margin: 0,
+          padding: 0,
+          verticalAlign: 'middle',
+        },
+        tooltip: {
+          headerFormat: '',
+          valueSuffix: '%',
+        },
+        credits: {
+          enabled: false, // Disable Highcharts.com tag
+        },
+        plotOptions: {
+          series: {
+            pointPadding: 0,
+            groupPadding: 0,
+            borderWidth: 1,
+            borderColor: '#1D2939',
+            dataLabels: {
+              enabled: true,
+              align: 'center',
+              format: `{y}%`,
+            },
+            stacking: 'percent',
+            paths: [
+              {
+                definition:
+                  'M206.452 216.735C231.945 194.011 248 160.915 248 124.067 248 55.547 192.483 0 124 0S0 55.547 0 124.067c0 36.664 15.895 69.613 41.166 92.325 8.496 17.804 14.781 35.067 15.346 54.453.246 8.451 6.678 15.92 15.134 15.92h105.832c8.58 0 15.082-7.689 15.312-16.266.474-17.711 5.863-35.274 13.662-53.764z',
+              },
+            ],
+          },
+        },
+        series: bulbHighData,
+      });
+    }
   }, []);
 
-  return <div id="bulbContainer" />;
+  return <div id="bulbContainer" style={{ height: '400px', width: '100%' }} />;
 };
